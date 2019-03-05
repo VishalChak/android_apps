3cf2e5a1dc3fb54db2a677c0d64b38ab755abb37
VISHAL BABU <vbabu@ap.equinix.com>
2019-03-06 00:02:13 +0800

    KOVISH APP

--- Kovish_app/app/src/main/java/com/example/kovish_app/DisplayImage.java
+++ Kovish_app/app/src/main/java/com/example/kovish_app/DisplayImage.java
@@ -4,7 +4,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class DisplayImage extends AppCompatActivity {
 
@@ -17,5 +19,9 @@ public class DisplayImage extends AppCompatActivity {
         imageView  = (ImageView) findViewById(R.id.imageView);
         Bitmap bitmap = BitmapFactory.decodeFile(getIntent().getStringExtra("image_path"));
         imageView.setImageBitmap(bitmap);
+    }
+
+    public void getCaption(View view) {
+        Toast.makeText(this, "Working on it", Toast.LENGTH_SHORT).show();
     }
 }
